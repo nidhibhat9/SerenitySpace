11628913b5386823f76b383eb9bf2cb2ea027209
Saanvi Jadhav <saanvij957@gmail.com>
2025-07-15 13:32:42 -0400

    Added Anxious Promt Questions Text

---
+++ SerenitySpace/Anxious.swift
@@ -0,0 +1,23 @@
+//
+//  Anxious.swift
+//  SerenitySpace
+//
+//  Created by Scholar on 7/15/25.
+//
+
+import SwiftUI
+
+struct Anxious: View {
+    var body: some View {
+        VStack {
+            Text("Anxious Prompt Questions")
+                .font(.title)
+                .fontWeight(.bold)
+            
+        }
+    }
+}
+
+#Preview {
+    Anxious()
+}
--- SerenitySpace/ContentView.swift
+++ SerenitySpace/ContentView.swift
@@ -9,15 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
         }
-        .padding()
     }
-}
 
 #Preview {
     ContentView()
