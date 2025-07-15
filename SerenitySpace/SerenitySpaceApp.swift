f20f66e0e0189816457c3a8f2c6e21d01688343c
Nidhi Bhat <nidhigbhat9@gmail.com>
2025-07-15 11:39:12 -0400

    Initial Commit

---
+++ SerenitySpace/SerenitySpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SerenitySpaceApp.swift
+//  SerenitySpace
+//
+//  Created by Scholar on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct SerenitySpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SerenitySpaceTests/SerenitySpaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  SerenitySpaceTests.swift
+//  SerenitySpaceTests
+//
+//  Created by Scholar on 7/15/25.
+//
+
+import Testing
+@testable import SerenitySpace
+
+struct SerenitySpaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
