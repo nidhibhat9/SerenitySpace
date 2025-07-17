6edd38058f1eef6afa8e0f4910f1ba5b56cb0226
Nidhi Bhat <nidhigbhat9@gmail.com>
2025-07-17 14:33:23 -0400

    Fixed parts of the code

--- SerenitySpace/Happy.swift
+++ SerenitySpace/Happy.swift
@@ -1,90 +1,75 @@
 //
-//  Happy.swift
+//  Sad.swift
 //  SerenitySpace
 //
 //  Created by Scholar on 7/16/25.
 //
 
 import SwiftUI
-
 struct Happy: View {
-    @State private var journalBox: String = ""
-    @State private var selectedPrompt: Int? = nil
-
+    @State private var showTextField: Int = 0
+    @State private var textenter: String = ""
     var body: some View {
-        ZStack {
-            Color(Color(hue: 0.842, saturation: 0.114, brightness: 0.973)).edgesIgnoringSafeArea(.all)
-            VStack {
-                Text("Glad to hear you're feeling Happy!")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color(hue: 0.777, saturation: 0.971, brightness: 0.532))
-                    .multilineTextAlignment(.center)
-                    .padding(40)
-
-                Spacer()
-
-                if selectedPrompt == nil {
-                    Text("Choose a prompt to journal about!")
-                        .font(.title)
-                        .fontWeight(.semibold)
+        NavigationStack {
+            ZStack {
+                Color(Color(hue: 0.842, saturation: 0.114, brightness: 0.973)).edgesIgnoringSafeArea(.all)
+                
+                VStack (alignment: .center, spacing: 30){
+                    Text("Glad to hear you're feeling happy!")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
                         .foregroundColor(Color.white)
                         .multilineTextAlignment(.center)
+                        .padding(.top)
                     
-
-                    Button("What made you smile today?") {
-                        selectedPrompt = 1
+                    Text("Choose a prompt to journal about!")
+                        .font(.title2)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.black)
+                    
+                    Button("1. What made you smile today?") {
+                        showTextField = 1
+                        textenter = ""
                     }
-                    .foregroundColor(Color(hue: 0.777, saturation: 0.961, brightness: 0.581))
-                    .padding(40)
-
-                    Button("What are you most grateful for?") {
-                        selectedPrompt = 2
+                    
+                    Button("2. What are you most grateful for?" ) {
+                        showTextField = 2
+                        textenter = ""
                     }
-                    .foregroundColor(Color(hue: 0.777, saturation: 0.961, brightness: 0.581))
-                    .padding(40)
-
-
-                    Button("What's a happy memory you cherish?") {
-                        selectedPrompt = 3
+                    
+                    Button("3. What's a happy memory you cherish?") {
+                        showTextField = 3
+                        textenter = ""
                     }
-                    .foregroundColor(Color(hue: 0.777, saturation: 0.961, brightness: 0.581))
-                    .padding(40)
-
-                }
-
-                if selectedPrompt != nil {
-                    Text("Great! Start Journaling!")
-                        .font(.headline)
-                        .fontWeight(.heavy)
-                        .foregroundColor(Color(hue: 0.772, saturation: 1.0, brightness: 0.52))
-                    TextField("Journal your answer here:", text: $journalBox)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    if showTextField != 0 {
+                        TextField("Answer prompt #\(showTextField) here...", text: $textenter)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.top,15)
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .padding(.horizontal)
+                    }
+                    Spacer()
                         .padding()
-
-                    NavigationLink(destination: Happy2()) {
-                        Text("Submit")
-                            .foregroundColor(Color.white)
-                            .padding()
-                            .background(Color(hue: 0.815, saturation: 0.418, brightness: 0.847))
-                            .cornerRadius(10)
-                            .font(.headline)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                    
+                    NavigationLink(destination: Sad2()){
+                        Text("SUBMIT")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.mint)
+                            
                     }
-
-                    Button("Back") {
-                        selectedPrompt = nil
-                        journalBox = ""
-                    }
-                    .foregroundColor(.black)
-                    .padding(.top)
+                    
+                    Spacer ()
+                    .padding(.bottom, 30)
+                    
                 }
-
-                Spacer()
             }
         }
     }
 }
-
 #Preview {
     Happy()
 }
+
