71ce79bdeb1cd7e3b73f672aa1a50d0e323cbb09
Aanya Kungwani <aanyak820@gmail.com>
2025-07-17 11:35:19 -0400

    fixed all of the sad page + added submit button to lead to next screen

---
+++ SerenitySpace/Sad2.swift
@@ -0,0 +1,18 @@
+//
+//  Sad2.swift
+//  SerenitySpace
+//
+//  Created by aanya kungwani on 7/17/25.
+//
+
+import SwiftUI
+
+struct Sad2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Sad2()
+}
