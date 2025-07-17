0c719e8df3d3e024803d23a73f6780e0e436238e
Nidhi Bhat <nidhigbhat9@gmail.com>
2025-07-17 10:46:14 -0400

    Added if statements

---
+++ SerenitySpace/Happy2.swift
@@ -0,0 +1,18 @@
+//
+//  Happy2.swift
+//  SerenitySpace
+//
+//  Created by Scholar on 7/17/25.
+//
+
+import SwiftUI
+
+struct Happy2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Happy2()
+}
